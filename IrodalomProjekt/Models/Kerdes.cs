426fa50633906d2fb97b6afbd2a96fa4b9f71545
sandorr <sandorr@kkszki.hu>
2025-02-17 09:06:56 +0100

    Kerdes osztály kidolgozva

--- IrodalomProjekt/Models/Kerdes.cs
+++ IrodalomProjekt/Models/Kerdes.cs
@@ -8,11 +8,31 @@ namespace IrodalomProjekt.Models
 {
     internal class Kerdes
     {
-        public string KerdesSzoveg { get; set; }
+        public Kerdes(string kerdesSzovege, string valaszA, string valaszB, string valaszC, string helyesValasz)
+        {
+            KerdesSzovege = kerdesSzovege;
+            ValaszA = valaszA;
+            ValaszB = valaszB;
+            ValaszC = valaszC;
+            HelyesValasz = helyesValasz;
+           
+        }
+
+        public string KerdesSzovege { get; set; }
         public string ValaszA { get; set; }
         public string ValaszB { get; set; }
         public string ValaszC { get; set; }
         public string HelyesValasz { get; set; }
-        public string FelhasznaloValasz { get; set; }
+        public string? FelhasznaloValasza { get; set; }
+
+        /// <summary>
+        /// A felhasználó válaszának ellenőrzése, ha nincs kitöltve, akkor a válasz automatikusan hibás.
+        /// </summary>
+        /// <returns></returns>
+        public bool ValaszEllenorzes()
+        { 
+            return FelhasznaloValasza is null? false: FelhasznaloValasza.ToLower()== HelyesValasz.ToLower();
+        }
+
     }
 }
